6bab04276366bc8a1fc1a5a20022cf7d16fea70a
Sagar1079 <sagarwankhade896@gmail.com>
2022-07-24 11:05:31 +0530

    FinalisedAll

--- navbar.js
+++ navbar.js
@@ -3,12 +3,12 @@ function navBar(){
             <div><a href="/find_Job/find_jobs.html"><img src="https://i.ibb.co/FBWSrg5/indeedkjgf.png" alt=""></a></div>
             <div class="find_blue"><a href="/find_Job/find_jobs.html">Find Jobs</a></div>
             <div class="company_blue"><a href="index.html">Comapny Reviews</a></div>
-            <div class="salary_blue"><a href="#">Salary Guide</a></div>
+            <div class="salary_blue"><a href="/salary_guide.html/salary_guide.html">Salary Guide</a></div>
         </div>
         <div>
             <div><a href="">Post your resume</a></div>
             <div id="signIndiv"><a href="/signup.html">Sign In</a></div>
-            <div><a href="">Employers / Post Job</a></div>
+            <div><a href="#">Employers / Post Job</a></div>
         </div>`
 }
         export default navBar;
