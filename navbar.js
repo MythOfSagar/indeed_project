1af04d0fb3a0b62b2411ce7d84146513849bec45
Rutujalomate <104217802+Rutujalomate@users.noreply.github.com>
2022-07-24 09:46:57 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,14 @@
+function navBar(){
+    return`<div>
+            <div><a href="/find_Job/find_jobs.html"><img src="https://i.ibb.co/FBWSrg5/indeedkjgf.png" alt=""></a></div>
+            <div class="find_blue"><a href="/find_Job/find_jobs.html">Find Jobs</a></div>
+            <div class="company_blue"><a href="#">Comapny Reviews</a></div>
+            <div class="salary_blue"><a href="#">Salary Guide</a></div>
+        </div>
+        <div>
+            <div><a href="">Post your resume</a></div>
+            <div id="signIndiv"><a href="/signup.html">Sign In</a></div>
+            <div><a href="">Employers / Post Job</a></div>
+        </div>`
+}
+        export default navBar;
